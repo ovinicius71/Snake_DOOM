6c701d4f2911dcfbe11b2008ec31697e671bf9e2
ovinicius71 <viniciusoliveiralima72@gmail.com>
2024-11-24 21:31:40 -0400

    continui sprite_object

--- sprite_object.py
+++ sprite_object.py
@@ -3,3 +3,16 @@ from settings import *
 import os
 from collections import deque
 
+class SprinteObject:
+    def __init__(self, game, path='assets/sprites/static_sprites/candlebra.png',pos = (10.5, 3.5), scale=0.7, shift=0.27):
+        self.game = game
+        self.player = game.player 
+        self.x,self.y = pos
+        self.image = pg.image.load(path).convert_alpha()
+        self.IMAGE_WIDTH = self.image.get_width()
+        self.IMAGE_HALF_WIDTH = self.image.get_width() // 2
+        self.IMAGE_RATIO = self.IMAGE_WIDTH / self.image.get_height()
+        self.dx, self.dy, self.theta, self.screen_x, self.dist, self.norm_dist = 0, 0, 0, 0, 1, 1
+        self.sprite_half_width = 0
+        self.SPRITE_SCALE = scale
+        self.SPRITE_HEIGHT_SHIFT = shift
