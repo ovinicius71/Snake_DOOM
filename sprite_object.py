a73905871dbd0b829eaab1e4aa5d9fd30509bd84
ovinicius71 <viniciusoliveiralima72@gmail.com>
2024-11-27 08:44:52 -0400

    finish get_sprite

--- sprite_object.py
+++ sprite_object.py
@@ -32,3 +32,15 @@ class SprinteObject:
         dy = self.y -self.player.y
         self.dx,self.dy = dx, dy
         self.theta = math.atan2(dy,dx)
+
+        delta = self.theta - self.player.angle
+        if (dx > 0 and self.player.angle > math.pi) or (dx < 0 and dy < 0):
+            delta += math.tau
+
+        delta_rays = delta / DELTA_ANGLE
+        self.screen_x = (HALF_NUM_RAYS + delta_rays) * SCALE
+
+        self.dist = math.hypot(dx, dy)
+        self.norm_dist = self.dist * math.cos(delta)
+        if -self.IMAGE_HALF_WIDTH < self.screen_x < (WIDTH + self.IMAGE_HALF_WIDTH) and self.norm_dist > 0.5:
+            self.get_sprite_projection()
