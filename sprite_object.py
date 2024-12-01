99af6dc04dfa8b70b86432074d066d98cbb47df6
ovinicius71 <viniciusoliveiralima72@gmail.com>
2024-11-30 21:48:36 -0400

    add function update sprite

--- sprite_object.py
+++ sprite_object.py
@@ -44,3 +44,8 @@ class SprinteObject:
         self.norm_dist = self.dist * math.cos(delta)
         if -self.IMAGE_HALF_WIDTH < self.screen_x < (WIDTH + self.IMAGE_HALF_WIDTH) and self.norm_dist > 0.5:
             self.get_sprite_projection()
+
+    def update(self):
+        self.get_sprite()
+
+class animatedSprite ()
